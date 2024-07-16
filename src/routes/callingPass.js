6adcc2fece1ac6397e9d4e555345b934cbbfdb3e
Carlos Samuel <93002480+AzuUmy@users.noreply.github.com>
2024-07-16 16:48:06 -0300

    :sparkles: add, option to delete called queue number from queue on calling

--- src/routes/callingPass.js
+++ src/routes/callingPass.js
@@ -36,13 +36,39 @@ module.exports = (wss) => {
             const { atendente, guiche } = ctx.query;
             const collectionName = 'currentCalling';
             const currentCallingS = getCallingModel(collectionName);
-
             const currentCSenha = await currentCallingS.find({ atendente, guiche });
-            
             ctx.body = { status: 'Success', data: currentCSenha };
         } catch (err){
             ctx.status = 500;
             ctx.body = {status: 'error', message: 'internal Server Error'};
+        }
+    });
+
+    router.delete('/deleteCall/:senha/:guiche/:atendente', async (ctx) => {
+        const { senha, guiche, atendente } = ctx.params;
+
+        try {
+            const collectionName = 'currentCalling';
+            const calledSenha = getCallingModel(collectionName);
+
+            const deleteCalledSenha = await calledSenha.findOneAndDelete({senha: parseInt(senha), guiche, atendente });
+
+         if(deleteCalledSenha) {
+            console.log("Deleting current",  senha,  "call from", atendente, guiche);
+            wss.clients.forEach(client => {
+                if(client.readyState === WebSocket.OPEN){
+                    client.send(JSON.stringify({ status: 'success', data: deleteCalledSenha}));
+                }
+            });
+            ctx.body = { status: 'success', data: deleteCalledSenha };
+         }   else{
+            ctx.body = { status: 'error', message: 'senha not founded'}
+         }
+
+        } catch (err){
+            ctx.status = 500;
+            ctx.body = {status: 'error', message: 'error', err}
+
         }
     });
 
